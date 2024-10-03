9961607b70b930cade7abadd0cf6088658fc097d
nzhong12 <NathanYZhong@gmail.com>
2024-10-02 22:37:26 -0400

    back to following along authors code, 63

--- src/App.jsx
+++ src/App.jsx
@@ -1,151 +1,88 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import PropTypes from 'prop-types'; 
-import './App.css';
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const welcome = {
-  greeting: "Salutations",
-  subject: "Web Developer",
-};
-
-const list = [
-  {
-    title: 'site 1, React',
-    url: 'https://reactjs.org/',
-    author: 'Y Zhong',
-    num_comments: 2,
-    points: 3,
-    objectID: 0,
-  }, 
-  {
-    title: 'site 2, Redux',
-    url: 'https://redux.js.org/',
-    author: 'A Booth, C Dean',
-    num_comments: 5,
-    points: 1,
-    objectID: 1,
-  },
-  {
-    title: 'site 3, Google',
-    url: 'https://Google.com/',
-    author: 'People',
-    num_comments: 100,
-    points: 101,
-    objectID: 1,
-  },
-];
-
-const list2 = [
-  {
-    title: 'site 1 clone, React',
-    url: 'https://reactjs.org/',
-    author: 'A B',
-    num_comments: 2,
-    points: 3,
-    objectID: 0,
-  }, 
-  {
-    title: 'site 2 clone, Redux',
-    url: 'https://redux.js.org/',
-    author: 'C D',
-    num_comments: 5,
-    points: 1,
-    objectID: 1,
-  },
-];
-
-List.propTypes = {
-  items: PropTypes.arrayOf( // prop validation for items which is used in list. This way .map won't complain
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      num_comments: PropTypes.number.isRequired,
-      points: PropTypes.number.isRequired,
-      objectID: PropTypes.number.isRequired,
-    })
-  ),
-};
-
-Item.propTypes = {
-  item: PropTypes.shape({ // prop validation for Item
-    title: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    num_comments: PropTypes.number.isRequired,
-    points: PropTypes.number.isRequired,
-    objectID: PropTypes.number.isRequired,
-  }),
-};
-
-function getNote(note) {
-  return note;
-}
-
-const App = () => { 
-  const [count, setCount] = useState(0); // must use () => {  because useState is a named export
-  const title = 'Kyrie';
-  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + {title}</h1>
-      <h2>{welcome.greeting} {welcome.subject}. {getNote('Welcome to React')}</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Search />
-        <hr />
-        {/* List component can have many instances like below, props are DIFFERENT LISTS. Instance of compoenent is element.*/}
-        <List items={list} />
-        <List items={list2} />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
-
-const List = (items) => ( // no bracket needed, IMPLICIT return statement exists
-    <ul>
-      {items.map((item) => ( /* this mapping is now in the List component, so now a new Item component can be extracted. List will 
-      loop through elements of given Items. This contrasts to before where all this work was done in one function */
-        <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-)
-
-function Item({ item }) {  
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>, {item.author}</span>
-      <span>, {item.num_comments}</span>
-      <span>, {item.points}</span>
-    </li>
-  );
-}
-
-const Search = () => (
     <div>
-      <label htmlFor="filter">Filter: </label>
-      <input id="filter" type="text" />
+      <h1>My Hacker Stories</h1>
+
+      <Search />
+      <Find />
+
+      <hr />
+ 
+      <List list={stories} /> {/* entire list of stories is passed as props to List*/}
     </div>
-)
+  );
+};
+
+const Search = () => {
+  const handleChange = (event) => { // notice how event handler function is concise with arrow function. Can have parametere with handleChange(param)])
+    //ALL the stuff below happens after search bar input CHANGES
+    // synthetic event, console object is recorded
+    console.log(event);
+    // value of target, CURRENT VALUE of input field is logged (here: input HTML element)
+    console.log(event.target.value);
+    // overall, handleChange  wraps event handler in a SyntheticEvent object to standardize the behavior across different browsers
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}/> {/* onChange = {}, event name is Change, handleChange is event handler function */}
+    </div>
+  );
+};
+
+const Find = () => {
+  const handleClick = (event) => { 
+    console.log(event);
+    // value of target, CURRENT VALUE of input field is logged (here: input HTML element)
+    console.log(event.target.value);
+    // overall, handleChange  wraps event handler in a SyntheticEvent object to standardize the behavior across different browsers
+  };
+
+  return (
+    <div>
+      <label htmlFor="find">Find: </label>
+      <input id="find" type="text" onClick={handleClick}/> {/* onClick = {}, event name is Click, handleClick is event handler function */}
+    </div>
+  );
+};
+
+const List = (props) => ( // introducing parameter LIST OF ITEMS is props. iterate thru props.list to get items to pass to Item
+  <ul>
+    {props.list.map((item) => ( // overall props flow from paretn to child, like list to item, etc. Unidirectional! 
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => ( // introducing parameter EACH ITEM is props. display properties of EACH item/story
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 export default App;
